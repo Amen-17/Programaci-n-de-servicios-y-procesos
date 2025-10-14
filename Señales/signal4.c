3d05c31aa84db2b9cd172b69b03c55fc56bb5f81
Amen-17 <amenelbou@gmail.com>
2025-10-14 10:16:31 +0200

    Add files via upload

--- Señales/signal4.c
+++ Señales/signal4.c
@@ -8,14 +8,14 @@
 void gestionaSenal(int num){
 	time_t tiempo = time(NULL);
 	struct tm *tm_info = localtime(&tiempo);
-printf("Señal de alarma recibida a las %02d:%02d:%02d");
+printf("Señal de alarma recibida a las %02d:%02d:%02d\n",tm_info->tm_hour,tm_info->tm_min, tm_info->tm_sec);
 }
 void main(){
 	int num1, num2;
-	printf("¿Cuántas veces sonará a alarma?");
-	scanf("%d",num1);
-	printf("¿Cada cuántos seugnod se repetirá la alarma?");
-	scanf("%d",num2);
+	printf("¿Cuántas veces sonará a alarma?\n");
+	scanf("%d",&num1);
+	printf("¿Cada cuántos segundos se repetirá la alarma?\n");
+	scanf("%d",&num2);
 	for(int i = 0; i < num1; i++){
 		signal(SIGUSR1,gestionaSenal);
 		kill(getpid(),SIGUSR1);
